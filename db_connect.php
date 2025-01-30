8813bd9b7992e4f8be093aab48aec7238793abc1
Neyamul_hasan <121274253+neyamul-hasan14@users.noreply.github.com>
2025-01-31 00:42:34 +0600

    Delete db_connect.php

--- db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	// connect to the database
-	$conn = mysqli_connect('localhost', 'root', '', 'DropEx');
-	// check connection
-	if(!$conn){
-		echo 'Connection error: '. mysqli_connect_error();
-	}
-?>	
